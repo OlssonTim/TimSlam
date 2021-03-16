2d31f95bd01d6c2ca87ab12498284dfc5dea377e
OlssonTim <tiol@csc.kth.se>
2021-03-16 23:51:30 +0100

    Normalize coords

--- extractor.py
+++ extractor.py
@@ -3,14 +3,26 @@ import numpy as np
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
 class Extractor(object):
     GX = 16//2
     GY = 12//2
 
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create()
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+    def normalize(self, pts):
+        return np.dot(self.Kinv, add_ones(pts).T).T[:, 0:2]
+
+    def denormalize(self, pt):
+        ret = np.dot(self.K, [pt[0], pt[1], 1.0])
+        return int(round(ret[0])), int(round(ret[1]))
 
     def extract(self, img):
         # Detection
@@ -30,12 +42,20 @@ class Extractor(object):
                     kp2 = self.last['kps'][m.trainIdx].pt
                     ret.append((kp1, kp2))
 
+
         # Filter
         if len(ret) > 0:
             ret = np.array(ret)
+            # Normalize coords: Subtract to move to 0
+            print(img.shape)
+            ret[:, 0, :] = self.normalize(ret[:, 0, :])
+            ret[:, 1, :] = self.normalize(ret[:, 1, :])
+            # ret[:, 1, :] = np.dot(self.Kinv, add_ones(ret[:,1,:]).T).T[:, 0:2]
 
             model, inliers = ransac((ret[:, 0], 
                                     ret[:, 1]), FundamentalMatrixTransform, min_samples=8, residual_threshold=1, max_trials=100)
+            
+            s,v,d = np.linalg.svd(model.params)
 
             ret = ret[inliers]
 
--- main.py
+++ main.py
@@ -7,9 +7,13 @@ import numpy as np
 W = 1920//2
 H = 1080//2
 
+F = 1
+
+K = np.array(([F, 0, W//2], [0,F,H//2], [0, 0, 1]))
+
 display = Display(W, H)
 
-fe = Extractor()
+fe = Extractor(K)
 
 def process_frame(img):
     img = cv2.resize(img, (W, H))
@@ -19,8 +23,9 @@ def process_frame(img):
         return
 
     for pt1, pt2 in matches:
-        u1, v1 = map(lambda x: int(round(x)), pt1)
-        u2, v2 = map(lambda x: int(round(x)), pt2)
+        u1, v1 = fe.denormalize(pt1)
+        u2, v2 = fe.denormalize(pt2)
+
         cv2.circle(img, (u1,v1), 3, (0,255,0))
         cv2.line(img, (u1,v1), (u2, v2), (255,0,0))
 
