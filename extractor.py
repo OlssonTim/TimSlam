21231a4da36550695cd8caf1b99b3603708c8650
OlssonTim <tiol@csc.kth.se>
2021-03-17 22:12:12 +0100

    Refactor to keep indices

--- extractor.py
+++ extractor.py
@@ -7,7 +7,11 @@ import g2o
 def add_ones(x):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
-def extractRt(E):
+
+IRt = np.eye(4)
+#IRt[:, :3] = np.eye(3)
+
+def extractPose(E):
     W = np.mat([[0,-1,0],[1,0,0],[0,0,1]])
     U,d,Vt = np.linalg.svd(E)
     assert np.linalg.det(U) > 0
@@ -17,8 +21,10 @@ def extractRt(E):
     if np.sum(R.diagonal()) < 0:
         R = np.dot(np.dot(U, W.T), Vt)
     t = U[:, 2]
-    Rt = np.concatenate([R,t.reshape(3,1)], axis=1)
-    return Rt
+    ret = np.eye(4)
+    ret[:3, :3] = R
+    ret[:3, 3] = t
+    return ret
 
 def extract(img):
     orb = cv2.ORB_create()
@@ -29,7 +35,6 @@ def extract(img):
     # Extraction
     kps = [cv2.KeyPoint(f[0][0], f[0][1], 20) for f in pts]
     kps, des = orb.compute(img, kps)
-
 
     return np.array([(kp.pt[0], kp.pt[1]) for kp in kps]), des
 
@@ -47,20 +52,26 @@ class Matcher(object):
         self.last = None
 
 
-def match(f1, f2):
+def match_frames(f1, f2):
     bf = cv2.BFMatcher(cv2.NORM_HAMMING)
     matches = bf.knnMatch(f1.des, f2.des, k=2)
 
     # Lowe's ratio test
     ret = []
+    idx1, idx2 = [], []
     for m, n in matches:
         if m.distance < 0.75*n.distance:
+            # Keep idxs
+            idx1.append(m.queryIdx)
+            idx2.append(m.trainIdx)
             p1 = f1.pts[m.queryIdx]
             p2 = f2.pts[m.trainIdx]
             ret.append((p1, p2))
 
     assert len(ret) >= 8
     ret = np.array(ret)
+    idx1 = np.array(idx1)
+    idx2 = np.array(idx2)
 
     # Fit matrix
     model, inliers = ransac((ret[:, 0], 
@@ -68,14 +79,16 @@ def match(f1, f2):
     
     # Ignore outliers
     ret = ret[inliers]
-    Rt = extractRt(model.params)
+    Rt = extractPose(model.params)
 
-    return ret, Rt
+    return idx1[inliers], idx2[inliers], Rt
 
 
 class Frame(object):
     def __init__(self, img, K):
         self.K = K
         self.Kinv = np.linalg.inv(self.K)
+        self.pose = IRt
+
         pts, self.des = extract(img)
         self.pts = normalize(self.Kinv, pts)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 from display import Display
-from extractor import Frame, denormalize, match
+from extractor import Frame, denormalize, match_frames, IRt
 import time
 import numpy as np
 import g2o
@@ -12,18 +12,52 @@ K = np.array(([F, 0, W//2], [0,F,H//2], [0, 0, 1]))
 
 display = Display(W, H)
 
+class Point(object):
+    # A Point is a 3-D point in the world
+    # Each point is observed in multiple frames
+
+    def __init__(self, loc):
+        self.frames = []
+        self.location = loc
+        self.idxs = []
+
+    def add_observation(self, frame, idx):
+        # Frame is the frame class
+        self.frames.append(frame)
+        self.idxs.append(idx)
+
+def triangulate(pose1, pose2, pts1, pts2):
+    return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
+
 frames = []
 def process_frame(img):
     img = cv2.resize(img, (W, H))
     frame = Frame(img, K)
     frames.append(frame)
-
     if len(frames) <= 1:
         return
 
-    ret, Rt = match(frames[-2], frames[-1])
+    idx1, idx2, Rt = match_frames(frames[-1], frames[-2])
+    frames[-1].pose = np.dot(Rt, frames[-2].pose)
 
-    for pt1, pt2 in ret:
+    pts4d = triangulate(frames[-1].pose, frames[-2].pose, frames[-1].pts[idx1], frames[-2].pts[idx2])
+    pts4d /= pts4d[:, 3:]
+
+
+    # Reject points without enough "Parallax"
+    # reject points behind the camera
+    #good_pts4d = (np.abs(pts4d[:, 3]) > 0.005 & (pts4d[:, 2] > 0))
+    good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:, 2] > 0)
+    #pts4d = pts4d[good_pts4d]
+
+    for i, p in enumerate(pts4d):
+        if not good_pts4d[i]:
+            continue
+        pt = Point(p)
+        pt.add_observation(frames[-1], i)
+        pt.add_observation(frames[-2], i)
+
+    for pt1, pt2 in zip(frames[-1].pts[idx1], frames[-2].pts[idx2]):
         u1, v1 = denormalize(K, pt1)
         u2, v2 = denormalize(K, pt2)
 
